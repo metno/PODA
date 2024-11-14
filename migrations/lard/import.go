a4150c07134086eb9c01599227e6290f2afdc605
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-11-14 16:51:14 +0100

    Create three separate slices inside parseData that can be fed directly to pgx.CopyFromRows

--- migrations/lard/import.go
+++ migrations/lard/import.go
@@ -9,29 +9,13 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// TODO: I'm not sure I like the interface solution
-type DataInserter interface {
-	InsertData(i int) ([]any, error)
-	Len() int
-}
-
-type TextInserter interface {
-	InsertText(i int) ([]any, error)
-	Len() int
-}
-
-type FlagInserter interface {
-	InsertFlags(i int) ([]any, error)
-	Len() int
-}
-
-func InsertData(ts DataInserter, pool *pgxpool.Pool, logStr string) (int64, error) {
-	size := ts.Len()
+func InsertData(ts [][]any, pool *pgxpool.Pool, logStr string) (int64, error) {
+	size := len(ts)
 	count, err := pool.CopyFrom(
 		context.TODO(),
 		pgx.Identifier{"public", "data"},
 		[]string{"timeseries", "obstime", "obsvalue"},
-		pgx.CopyFromSlice(size, ts.InsertData),
+		pgx.CopyFromRows(ts),
 	)
 	if err != nil {
 		return count, err
@@ -46,13 +30,13 @@ func InsertData(ts DataInserter, pool *pgxpool.Pool, logStr string) (int64, erro
 	return count, nil
 }
 
-func InsertTextData(ts TextInserter, pool *pgxpool.Pool, logStr string) (int64, error) {
-	size := ts.Len()
+func InsertTextData(ts [][]any, pool *pgxpool.Pool, logStr string) (int64, error) {
+	size := len(ts)
 	count, err := pool.CopyFrom(
 		context.TODO(),
 		pgx.Identifier{"public", "nonscalar_data"},
 		[]string{"timeseries", "obstime", "obsvalue"},
-		pgx.CopyFromSlice(size, ts.InsertText),
+		pgx.CopyFromRows(ts),
 	)
 	if err != nil {
 		return count, err
@@ -67,13 +51,13 @@ func InsertTextData(ts TextInserter, pool *pgxpool.Pool, logStr string) (int64, 
 	return count, nil
 }
 
-func InsertFlags(ts FlagInserter, table pgx.Identifier, columns []string, pool *pgxpool.Pool, logStr string) error {
-	size := ts.Len()
+func InsertFlags(ts [][]any, pool *pgxpool.Pool, logStr string) error {
+	size := len(ts)
 	count, err := pool.CopyFrom(
 		context.TODO(),
-		table,
-		columns,
-		pgx.CopyFromSlice(size, ts.InsertFlags),
+		pgx.Identifier{"flags", "old_databases"},
+		[]string{"timeseries", "obstime", "corrected", "controlinfo", "useinfo", "cfailed"},
+		pgx.CopyFromRows(ts),
 	)
 	if err != nil {
 		return err
