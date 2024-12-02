4055dbe40aa7858c23a1ba2622c610e7e319b6c2
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-12-02 13:22:43 +0100

    Rework comments

--- migrations/kvalobs/dump/main.go
+++ migrations/kvalobs/dump/main.go
@@ -5,16 +5,9 @@ import (
 	"migrate/utils"
 )
 
-// Same timeseries could be in both 'data' and 'text_data' tables
-// First of all, why?
-// Second, do we care?
-// func readDataAndText(label *lard.Label, pool *pgxpool.Pool, config *DumpConfig) Data {
-//     // Supposed to join text anf number data to single slice
-//     return nil
-// }
-//
-// TODO: not sure what to do with this one
-// func joinTS(first, second []lard.Label)
+// TODO: there were some comments in the original script about
+// the fact that the same timeseries could be in both
+// 'data' and 'text_data'
 
 type Config struct {
 	db.BaseConfig
