bb391308cecb4fc654fad6f49e83189a7be0424b
Ingrid <git@ingrids.email>
2024-12-11 17:24:21 +0100

    db: flag tables for confident flags

--- db/flags.sql
+++ db/flags.sql
@@ -1,5 +1,29 @@
 CREATE SCHEMA IF NOT EXISTS flags;
 
+CREATE TABLE IF NOT EXISTS flags.confident (
+    timeseries INT4 NOT NULL,
+    obstime TIMESTAMPTZ NOT NULL,
+    usable BOOLEAN NOT NULL,
+    CONSTRAINT unique_confident_timeseries_obstime UNIQUE (timeseries, obstime),
+    -- TODO: should this and confident_providence fk into public.data?
+    CONSTRAINT fk_confident_timeseries FOREIGN KEY (timeseries) REFERENCES public.timeseries
+) PARTITION BY RANGE (obstime);
+CREATE INDEX IF NOT EXISTS confident_timestamp_index ON flags.confident (obstime);
+CREATE INDEX IF NOT EXISTS confident_timeseries_index ON flags.confident USING HASH (timeseries);
+
+CREATE TABLE IF NOT EXISTS flags.confident_provenance (
+    timeseries INT4 NOT NULL,
+    obstime TIMESTAMPTZ NOT NULL,
+    pipeline TEXT NOT NULL,
+    -- TODO: should this be an enum?
+    flag INT4 NOT NULL,
+    fail_condition TEXT NULL,
+    CONSTRAINT unique_confident_providence_timeseries_obstime_pipeline UNIQUE (timeseries, obstime, pipeline),
+    CONSTRAINT fk_confident_providence_timeseries FOREIGN KEY (timeseries) REFERENCES public.timeseries
+) PARTITION BY RANGE (obstime);
+CREATE INDEX IF NOT EXISTS confident_provenance_timestamp_index ON flags.confident_providence (obstime);
+CREATE INDEX IF NOT EXISTS confident_provenance_timeseries_index ON flags.confident_providence USING HASH (timeseries);
+
 CREATE TABLE IF NOT EXISTS flags.kvdata (
     timeseries INT4 REFERENCES public.timeseries,
     obstime TIMESTAMPTZ NOT NULL,
