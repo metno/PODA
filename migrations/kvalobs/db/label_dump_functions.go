1445cbf0b01a2500a732cf3d21c4e33d3eab077a
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-12-11 12:34:27 +0100

    Improve label dumps (final)

--- migrations/kvalobs/db/label_dump_functions.go
+++ migrations/kvalobs/db/label_dump_functions.go
@@ -13,11 +13,6 @@ import (
 
 // Here are implemented the `LabelDumpFunc` stored inside the Table struct
 
-const OBSERVATIONS_QUERY string = `SELECT DISTINCT stationid, typeid FROM observations
-WHERE ($1::timestamp IS NULL OR obstime >= $1)
-  AND ($2::timestamp IS NULL OR obstime < $2)
-ORDER BY stationid`
-
 const OBSDATA_QUERY string = `SELECT DISTINCT paramid, sensor::int, level FROM obsdata
 JOIN observations USING(observationid)
 WHERE stationid = $1
@@ -37,41 +32,53 @@ type StationType struct {
 	typeid    int32
 }
 
-func GetUniqueStationsAndTypeIds(timespan *utils.TimeSpan, pool *pgxpool.Pool) ([]*StationType, error) {
-	rows, err := pool.Query(context.TODO(), OBSERVATIONS_QUERY, timespan.From, timespan.To)
-	if err != nil {
-		return nil, err
+// Lazily initialized slice of distinct stationids and typeids from the `observations` table
+var UNIQUE_STATIONS_TYPES []*StationType = nil
+
+func initUniqueStationsAndTypeIds(timespan *utils.TimeSpan, pool *pgxpool.Pool) error {
+	if UNIQUE_STATIONS_TYPES != nil {
+		return nil
 	}
 
-	labels := make([]*StationType, 0, rows.CommandTag().RowsAffected())
-	return pgx.AppendRows(labels, rows, func(row pgx.CollectableRow) (*StationType, error) {
+	rows, err := pool.Query(context.TODO(),
+		`SELECT DISTINCT stationid, typeid FROM observations
+            WHERE ($1::timestamp IS NULL OR obstime >= $1)
+              AND ($2::timestamp IS NULL OR obstime < $2)
+            ORDER BY stationid`,
+		timespan.From, timespan.To)
+	if err != nil {
+		return err
+	}
+
+	UNIQUE_STATIONS_TYPES = make([]*StationType, 0, rows.CommandTag().RowsAffected())
+	UNIQUE_STATIONS_TYPES, err = pgx.AppendRows(UNIQUE_STATIONS_TYPES, rows, func(row pgx.CollectableRow) (*StationType, error) {
 		var label StationType
 		err := row.Scan(&label.stationid, &label.typeid)
 		return &label, err
 	})
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func dumpDataLabels(timespan *utils.TimeSpan, pool *pgxpool.Pool) ([]*Label, error) {
+func dumpDataLabels(timespan *utils.TimeSpan, pool *pgxpool.Pool, maxConn int) ([]*Label, error) {
 	// First query stationid and typeid from observations
 	// Then query paramid, sensor, level from obsdata
 	// This is faster than querying all of them together from data
-
-	slog.Info("Querying data labels labels...")
-	// TODO: this should be done outside (can be reused for data and text)
-	stationsAndTypes, err := GetUniqueStationsAndTypeIds(timespan, pool)
-	if err != nil {
+	slog.Info("Querying data labels...")
+	if err := initUniqueStationsAndTypeIds(timespan, pool); err != nil {
 		slog.Error(err.Error())
 		return nil, err
 	}
 
-	bar := utils.NewBar(len(stationsAndTypes), "Data labels")
+	bar := utils.NewBar(len(UNIQUE_STATIONS_TYPES), "Stations")
 	var labels []*Label
 	var wg sync.WaitGroup
 
-	// TODO: can we move this somewhere else?
-	semaphore := make(chan struct{}, 4)
-
-	for _, s := range stationsAndTypes {
+	semaphore := make(chan struct{}, maxConn)
+	for _, s := range UNIQUE_STATIONS_TYPES {
 		wg.Add(1)
 		semaphore <- struct{}{}
 
@@ -109,26 +116,22 @@ func dumpDataLabels(timespan *utils.TimeSpan, pool *pgxpool.Pool) ([]*Label, err
 	return labels, nil
 }
 
-func dumpTextLabels(timespan *utils.TimeSpan, pool *pgxpool.Pool) ([]*Label, error) {
+func dumpTextLabels(timespan *utils.TimeSpan, pool *pgxpool.Pool, maxConn int) ([]*Label, error) {
 	// First query stationid and typeid from observations
-	// Then query paramid, sensor, level from obsdata
+	// Then query paramid from obstextdata
 	// This is faster than querying all of them together from data
-
-	slog.Info("Querying data labels labels...")
-	// TODO: this should be done outside (can be reused for data and text)
-	stationsAndTypes, err := GetUniqueStationsAndTypeIds(timespan, pool)
-	if err != nil {
+	slog.Info("Querying text labels...")
+	if err := initUniqueStationsAndTypeIds(timespan, pool); err != nil {
 		slog.Error(err.Error())
 		return nil, err
 	}
 
-	bar := utils.NewBar(len(stationsAndTypes), "Text labels")
+	bar := utils.NewBar(len(UNIQUE_STATIONS_TYPES), "Stations")
 	var labels []*Label
 	var wg sync.WaitGroup
 
-	// TODO: can we move this somewhere else?
-	semaphore := make(chan struct{}, 4)
-	for _, s := range stationsAndTypes {
+	semaphore := make(chan struct{}, maxConn)
+	for _, s := range UNIQUE_STATIONS_TYPES {
 		wg.Add(1)
 		semaphore <- struct{}{}
 
