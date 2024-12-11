0ac0890c00b139a3c09526265eb3f62b9e81a7e5
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-12-11 13:32:21 +0100

    Update check command

--- migrations/stinfosys/non_scalars.go
+++ migrations/stinfosys/non_scalars.go
@@ -7,7 +7,7 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func getNonScalars(conn *pgx.Conn) []int32 {
+func GetNonScalars(conn *pgx.Conn) []int32 {
 	rows, err := conn.Query(context.TODO(), "SELECT paramid FROM param WHERE scalar = false ORDER BY paramid")
 	if err != nil {
 		log.Fatal(err)
@@ -17,28 +17,4 @@ func getNonScalars(conn *pgx.Conn) []int32 {
 		log.Fatal(err)
 	}
 	return nonscalars
-}
-
-// Tells if a paramid is scalar or not
-type ScalarMap = map[int32]bool
-
-func GetParamScalarMap(conn *pgx.Conn) ScalarMap {
-	cache := make(ScalarMap)
-
-	rows, err := conn.Query(context.TODO(), "SELECT paramid, scalar FROM param")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for rows.Next() {
-		var paramid int32
-		var isScalar bool
-		if err := rows.Scan(&paramid, &isScalar); err != nil {
-			log.Fatal(err)
-		}
-		cache[paramid] = isScalar
-	}
-
-	return cache
-
 }
